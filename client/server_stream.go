cd2b3b3696b270c870162221365ff9688451e5dc
adil <aadilmubarake@gmail.com>
2024-11-14 16:51:07 +0530

    grpc-demo

---
+++ client/server_stream.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	pb "grpc-demo/proto"
+	"io"
+	"log"
+)
+
+func callSayHelloServerStream(client pb.GreetServiceClient, names *pb.NameList) {
+	log.Printf("Streaming started")
+	stream, err := client.SayHelloServerStreaming(context.Background(), names)
+	if err != nil {
+		log.Fatalf("could not send names: %v", err)
+	}
+
+	for {
+		message, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while streaming: %v", err)
+		}
+		log.Println(message)
+	}
+	log.Print("streaming finished")
+}
---
+++ server/server_stream.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	pb "grpc-demo/proto"
+	"log"
+	"time"
+)
+
+func (S *helloServer) SayHelloServerStreaming(req *pb.NameList, stream pb.GreetService_SayHelloServerStreamingServer) error{
+	log.Printf("got request with names: %v", req.Names)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello" + name,
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return nil
+}
